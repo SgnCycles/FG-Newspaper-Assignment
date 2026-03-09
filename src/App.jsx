48a4179bd135dd03497dcf2da85b462a29767f79
SgnCycles <signe.bekere@gmail.com>
2026-03-09 10:17:04 +0100

    fix headers

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,28 @@ import Footer from './components/Footer'
 import Main from './components/Main'
 import { articles } from './data/data.js'
 import randomArticle from './utils/shuffle.js'
-import removeUsedArticles from './utils/removeArticle.js'
 
 function App() {
   
   const newsCategories = ['Sport', 'Sweden', 'Crime', 'World', 'Entertainment'];
+  const normalizedCategories = newsCategories.map(category => category.toLowerCase());
 
   let newsArray = Array.from(articles);
   const mainArticle = randomArticle(newsArray);
 
   const noSportsArticles = newsArray.filter(article => article.category !== 'sport');
-
   const recentNews = [randomArticle(noSportsArticles), randomArticle(noSportsArticles), randomArticle(noSportsArticles), randomArticle(noSportsArticles)];
-
-  newsArray = removeUsedArticles(newsArray, recentNews);
   
   const sportsArticles = newsArray.filter(article => article.category === 'sport');
   const heroSportNews = [randomArticle(sportsArticles), randomArticle(sportsArticles)];
-
-  newsArray = removeUsedArticles(newsArray, heroSportNews);
-
-  const normalizedCategories = newsCategories.map(category => category.toLowerCase());
   
   const categorySection = normalizedCategories.map(category => {
     const categoryArticles = newsArray.filter(article => article.category === category);
     const categorySectionArticles = categoryArticles.slice(0, 3);
-    newsArray = removeUsedArticles(newsArray, categorySectionArticles)
     return {category, articles: categorySectionArticles};
   });
 
   const breakingNewsSection = newsArray.splice(0, 3);
-
-  newsArray = removeUsedArticles(newsArray, breakingNewsSection);
 
   return (
     <>
